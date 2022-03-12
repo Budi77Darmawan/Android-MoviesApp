70f9a6c31a989435a9fc40824648441ba24db72e
Budi77Darmawan <budymitman_77@yahoo.co.id>
2022-03-12 23:13:58 +0700

    add encryption on database with sqlcipher

--- core/src/main/java/com/bd_drmwan/core/module/DatabseModule.kt
+++ core/src/main/java/com/bd_drmwan/core/module/DatabseModule.kt
@@ -2,6 +2,7 @@ package com.bd_drmwan.core.module
 
 import android.content.Context
 import androidx.room.Room
+import com.bd_drmwan.core.BuildConfig.SQLCIPHER_KEY
 import com.bd_drmwan.core.main.data.locale.dao.MoviesDao
 import com.bd_drmwan.core.main.data.locale.dao.MoviesRoomDatabase
 import dagger.Module
@@ -9,6 +10,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,11 +20,16 @@ object DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): MoviesRoomDatabase =
-        Room.databaseBuilder(
+    fun provideDatabase(@ApplicationContext context: Context): MoviesRoomDatabase {
+        val passphrase = SQLiteDatabase.getBytes(SQLCIPHER_KEY.toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
             context,
             MoviesRoomDatabase::class.java, "Movies.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
 
     @Provides
     fun provideMoviesDao(database: MoviesRoomDatabase): MoviesDao = database.moviesDao()
