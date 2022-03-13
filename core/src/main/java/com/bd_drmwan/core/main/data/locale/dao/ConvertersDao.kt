d6209ac6d6ec42e79c67ab06923e50d34ce80cc9
Budi77Darmawan <budymitman_77@yahoo.co.id>
2022-03-14 04:33:31 +0700

    add default background on themes.xml

--- core/src/main/java/com/bd_drmwan/core/main/data/locale/dao/ConvertersDao.kt
+++ core/src/main/java/com/bd_drmwan/core/main/data/locale/dao/ConvertersDao.kt
@@ -6,15 +6,6 @@ import com.bd_drmwan.core.main.data.locale.entity.ListGenre
 import com.google.gson.Gson
 
 class ConvertersDao {
-    @TypeConverter
-    fun toString(list: List<String>?): String? {
-        return list?.joinToString(separator = ",")
-    }
-
-    @TypeConverter
-    fun toList(str: String?): List<String>? {
-        return str?.split(",")
-    }
 
     @TypeConverter
     fun toStringFromGenre(genre: List<GenreEntity>): String {
